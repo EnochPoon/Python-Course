d1948beeedd5e4db4febb8c0c3b1cd7dd3ad8490
Enoch Poon <enoch.poon99@gmail.com>
2020-04-12 19:46:12 -0400

    Updates

---
+++ precache-manifest.5f5d7c4dd93e29e23ca71169ead28f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0eb0d66288d91b46b485a4645be04c3",
+    "url": "/Python-Course/index.html"
+  },
+  {
+    "revision": "3d4e8ea7f9b994cf7d59",
+    "url": "/Python-Course/static/css/main.5d2c42b0.chunk.css"
+  },
+  {
+    "revision": "041c4f4c9c44d5d21f27",
+    "url": "/Python-Course/static/js/2.d174888e.chunk.js"
+  },
+  {
+    "revision": "5ced2f2ce7b79285485a360b83230d1b",
+    "url": "/Python-Course/static/js/2.d174888e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4e8ea7f9b994cf7d59",
+    "url": "/Python-Course/static/js/main.f171b7b2.chunk.js"
+  },
+  {
+    "revision": "c468133cf40093ae9e8d",
+    "url": "/Python-Course/static/js/runtime-main.e54eb8bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Python-Course/precache-manifest.26af17a005b31dffacc3db7a8436d7b5.js"
+  "/Python-Course/precache-manifest.5f5d7c4dd93e29e23ca71169ead28f38.js"
 );
 
 self.addEventListener('message', (event) => {
